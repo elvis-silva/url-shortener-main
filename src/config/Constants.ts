9f8676c6f1ed2795e06a2bebe150e8cf11b0833c
Elvis Silva <elviscarlossouza@gmail.com>
2022-01-26 12:14:40 -0300

    commit

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,4 +1,4 @@
 export const config = {
   API_URL: 'http://localhost:5000',
-  MONGO_CONNECTION: 'mongodb+srv://elvissilva:hJYZ2uVkFkrvxWP@cluster0.gt6bp.mongodb.net/Cluster0?retryWrites=true&w=majority'
+  MONGO_CONNECTION: 'mongodb+srv://<USER-NAME>:<PASSWORD>@cluster0.gt6bp.mongodb.net/<NOME-DO-CLUSTER>?retryWrites=true&w=majority'
 }
